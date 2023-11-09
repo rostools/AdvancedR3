21cbe3eac601d79d3d0e9c5e6c39c45ea185d4d8
Roger Moreno <roger.moreno.justicia@sund.ku.dk>
2023-11-09 11:53:53 +0100

    Worked on the previously created pipeline

--- R/functions.R
+++ R/functions.R
@@ -10,3 +10,17 @@ descriptive_stats <- function(data) {
         dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
         dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~round(.x, digits = 1)))
 }
+
+## This should be in the R/functions.R file.
+#' Plot for basic distribution of metabolite data.
+#'
+#' @param data The lipidomics dataset.
+#'
+#' @return A ggplot2 graph.
+#'
+plot_distributions <- function(data) {
+    data %>%
+        ggplot2::ggplot(ggplot2::aes(x = value)) +
+        ggplot2::geom_histogram() +
+        ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+}
