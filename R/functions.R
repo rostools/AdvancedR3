c9adb70176c5898af72b959985394459b6022c9a
Anders Askeland <anders.askeland@gmail.com>
2025-12-04 10:59:16 +0100

    Moved fit_all_models to functions file

--- R/functions.R
+++ R/functions.R
@@ -90,3 +90,18 @@ create_model_results <- function(data) {
     preprocess() |>
     fit_model(class ~ value)
 }
+
+#' Fit all models to a given data frame.
+#'
+#' @param data The data frame to fit the models to
+#'
+#' @returns A data frame with results from all models.
+#'
+fit_all_models <- function(data) {
+  list(
+    class ~ value,
+    class ~ value + gender + age
+  ) |>
+    purrr::map(\(model) fit_model(data, model = model)) |>
+    purrr::list_rbind()
+}
